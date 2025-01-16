0de15bb8565df85214a551455ac8129aab0e779d
Pomázi Krisztián <krisztianpomazi@gmail.com>
2025-01-16 11:31:43 +0100

    Update RawNet2Spoof.py

--- models/RawNet2Spoof.py
+++ models/RawNet2Spoof.py
@@ -191,7 +191,7 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
         return out
     
-class Model(nn.Module):
+class Model2(nn.Module):
     def __init__(self, d_args):
         super().__init__()
 
@@ -275,7 +275,7 @@ class Model(nn.Module):
         output = self.logsoftmax(x)
         return output, last_hidden
 
-class Model2(nn.Module):
+class Model(nn.Module):
     #def __init__(self, d_args, device):
     def __init__(self, d_args):
         super().__init__()
@@ -304,25 +304,6 @@ class Model2(nn.Module):
         self.block5 = nn.Sequential(
             Residual_block(nb_filts=d_args["filts"][2]))
         self.avgpool = nn.AdaptiveAvgPool1d(1)
-
-        self.fc_attention0 = self._make_attention_fc(
-            in_features=d_args["filts"][1][-1],
-            l_out_features=d_args["filts"][1][-1])
-        self.fc_attention1 = self._make_attention_fc(
-            in_features=d_args["filts"][1][-1],
-            l_out_features=d_args["filts"][1][-1])
-        self.fc_attention2 = self._make_attention_fc(
-            in_features=d_args["filts"][2][-1],
-            l_out_features=d_args["filts"][2][-1])
-        self.fc_attention3 = self._make_attention_fc(
-            in_features=d_args["filts"][2][-1],
-            l_out_features=d_args["filts"][2][-1])
-        self.fc_attention4 = self._make_attention_fc(
-            in_features=d_args["filts"][2][-1],
-            l_out_features=d_args["filts"][2][-1])
-        self.fc_attention5 = self._make_attention_fc(
-            in_features=d_args["filts"][2][-1],
-            l_out_features=d_args["filts"][2][-1])
 
         self.bn_before_gru = nn.BatchNorm1d(
             num_features=d_args["filts"][2][-1])
@@ -356,54 +337,13 @@ class Model2(nn.Module):
         x = self.first_bn(x)
         x = self.selu(x)
 
-        x0 = self.block0(x)
-        y0 = self.avgpool(x0).view(x0.size(0),
-                                   -1)  # torch.Size([batch, filter])
-        y0 = self.fc_attention0(y0)
-        y0 = self.sig(y0).view(y0.size(0), y0.size(1),
-                               -1)  # torch.Size([batch, filter, 1])
-        x = x0 * y0 + y0  # (batch, filter, time) x (batch, filter, 1)
-
-        x1 = self.block1(x)
-        y1 = self.avgpool(x1).view(x1.size(0),
-                                   -1)  # torch.Size([batch, filter])
-        y1 = self.fc_attention1(y1)
-        y1 = self.sig(y1).view(y1.size(0), y1.size(1),
-                               -1)  # torch.Size([batch, filter, 1])
-        x = x1 * y1 + y1  # (batch, filter, time) x (batch, filter, 1)
-
-        x2 = self.block2(x)
-        y2 = self.avgpool(x2).view(x2.size(0),
-                                   -1)  # torch.Size([batch, filter])
-        y2 = self.fc_attention2(y2)
-        y2 = self.sig(y2).view(y2.size(0), y2.size(1),
-                               -1)  # torch.Size([batch, filter, 1])
-        x = x2 * y2 + y2  # (batch, filter, time) x (batch, filter, 1)
-
-        x3 = self.block3(x)
-        y3 = self.avgpool(x3).view(x3.size(0),
-                                   -1)  # torch.Size([batch, filter])
-        y3 = self.fc_attention3(y3)
-        y3 = self.sig(y3).view(y3.size(0), y3.size(1),
-                               -1)  # torch.Size([batch, filter, 1])
-        x = x3 * y3 + y3  # (batch, filter, time) x (batch, filter, 1)
-
-        x4 = self.block4(x)
-        y4 = self.avgpool(x4).view(x4.size(0),
-                                   -1)  # torch.Size([batch, filter])
-        y4 = self.fc_attention4(y4)
-        y4 = self.sig(y4).view(y4.size(0), y4.size(1),
-                               -1)  # torch.Size([batch, filter, 1])
-        x = x4 * y4 + y4  # (batch, filter, time) x (batch, filter, 1)
-
-        x5 = self.block5(x)
-        y5 = self.avgpool(x5).view(x5.size(0),
-                                   -1)  # torch.Size([batch, filter])
-        y5 = self.fc_attention5(y5)
-        y5 = self.sig(y5).view(y5.size(0), y5.size(1),
-                               -1)  # torch.Size([batch, filter, 1])
-        x = x5 * y5 + y5  # (batch, filter, time) x (batch, filter, 1)
-
+        x = self.block0(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        
         x = self.bn_before_gru(x)
         x = self.selu(x)
         x = x.permute(0, 2, 1)  # (batch, filt, time) >> (batch, time, filt)
